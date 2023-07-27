d809629e50bafdb99d30d7e08830018298ed4617
AbdulazizDevH <mr.abdulaziz00791@gmal.com>
2023-07-27 16:51:14 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-orange-600">
+    <Header />
+  </div>
+</template>
+<script>
+import { Header } from "./components";
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Header from './header/Header.vue';
+import Sidebar from './sidebar/Sidebar.vue'
+
+export { Header, Sidebar }
