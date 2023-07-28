81b4ef9b782db18956fb3d1ee0ebbe014fcfc452
AbdulazizDevH <mr.abdulaziz00791@gmal.com>
2023-07-28 17:31:53 +0500

    Added icons

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -13,17 +13,17 @@
 
       <div class="navbar__item">
         <div class="navbar__search w-[43] flex items-center justify-between pl-4 relative border border-solid border-[#ccc] rounded-[40px] ">
-          <input type="text" placeholder="Введите запрос">
+          <input class="w-full inline-block mr-7 bg-transparent outline-none border-none text-base font-normal" type="text" placeholder="Введите запрос">
           <span>
-            <svg class="" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z" class="style-scope yt-icon"></path></g></svg>
+            <svg class="w-[20px!important]" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z" class="style-scope yt-icon"></path></g></svg>
           </span>
           <img src="./assets/keyboard.png" alt="">
           <button>
             <svg class="" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z" class="style-scope yt-icon"></path></g></svg>
           </button>
         </div>
-        <div class="navbar__voice-search">
-          <svg class="" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M12 3C10.34 3 9 4.37 9 6.07V11.93C9 13.63 10.34 15 12 15C13.66 15 15 13.63 15 11.93V6.07C15 4.37 13.66 3 12 3ZM18.5 12H17.5C17.5 15.03 15.03 17.5 12 17.5C8.97 17.5 6.5 15.03 6.5 12H5.5C5.5 15.24 7.89 17.93 11 18.41V21H13V18.41C16.11 17.93 18.5 15.24 18.5 12Z" class="style-scope yt-icon"></path></g></svg>
+        <div class="navbar__voice-search w-10 h-10 ml-2 flex items-center justify-center rounded-full cursor-pointer">
+          <svg class="w-[24px!important] h-[24px!important]" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><g class="style-scope yt-icon"><path d="M12 3C10.34 3 9 4.37 9 6.07V11.93C9 13.63 10.34 15 12 15C13.66 15 15 13.63 15 11.93V6.07C15 4.37 13.66 3 12 3ZM18.5 12H17.5C17.5 15.03 15.03 17.5 12 17.5C8.97 17.5 6.5 15.03 6.5 12H5.5C5.5 15.24 7.89 17.93 11 18.41V21H13V18.41C16.11 17.93 18.5 15.24 18.5 12Z" class="style-scope yt-icon"></path></g></svg>
         </div>
       </div>
 
@@ -58,30 +58,7 @@ export default {
 
 
   &__search {
-    // @include flexbox(center, space-between);
-    // @include width-height(43vw, auto);
-    // border: 1px solid #ccc;
     box-shadow: inset 0 1px 2px #eee;
-    // border-radius: 40px;
-    // padding-left: 16px;
-    // position: relative;
-
-    input {
-      display: inline-block;
-      width: 100%;
-      margin-right: 28px;
-      background-color: transparent;
-      outline: none;
-      border: none;
-      font-size: 16px;
-      line-height: 24px;
-      font-weight: 400;
-    }
-    span {
-      svg {
-        width: 20px !important;
-      }
-    }
     span {
       display: none;
       // @include width-height(auto, 40px);
@@ -116,27 +93,13 @@ export default {
 
 
   &__voice-search {
-    // @include width-height(40px, 40px);
-    width: 40px;
-    height: 40px;
-    // @extend %nav-item;
-    cursor: pointer;
-    border-radius: 100%;
     transition: background-color 0.2s linear;
-    border: 1px solid transparent;
     &:hover {
       background-color: rgba(0, 0, 0, 0.1);
     }
     &:active {
       background-color: rgba(0, 0, 0, 0.2);
       border: 1px solid rgba(0, 0, 0, 0.2);
-    }
-    // @include flexbox(center, center);
-    margin-left: 8px;
-    svg {
-      // @include width-height(24px !important, 24px !important);
-      width: 24px !important; 
-      height: 24px !important;
     }
   }
 }
